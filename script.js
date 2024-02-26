b5cd3105572ce9ddb9f3d6de83eb09047d783afb
vishwamohini <vishwamohini0015@gmail.com>
2024-02-26 16:19:29 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var a=document.querySelector("#elem")
+let b =a.getAttribute("data-name")
+console.log(b);
