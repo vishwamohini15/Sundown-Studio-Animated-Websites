bd22b9ffe3da684e9d4f8ad569da41f51361cf89
vishwamohini <vishwamohini0015@gmail.com>
2024-02-28 15:21:59 +0000

    second commit

--- script.js
+++ script.js
@@ -4,6 +4,38 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-var a=document.querySelector("#elem")
-let b =a.getAttribute("data-name")
-console.log(b);
+// var a=document.querySelector("#elem1")
+// var image=a.getAttribute("data-image")
+// console.log(image);
+
+function page4Animation(){
+    var elemc=document.querySelector("#elem-container")
+var fixed=document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter", ()=>{
+    fixed.style.display="block";
+
+})
+elemc.addEventListener("mouseleave", ()=>{
+    fixed.style.display="none";
+    
+})
+
+var elems=document.querySelectorAll(".elem")
+elems.forEach((e)=>{
+    e.addEventListener("mouseenter",()=>{
+        var image=e.getAttribute("data-image")
+     fixed.style.backgroundImage=`url(${image})`
+    })
+})
+}
+
+function swiperAnimation(){
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 70,
+    
+  });
+}
+swiperAnimation()
+page4Animation()
